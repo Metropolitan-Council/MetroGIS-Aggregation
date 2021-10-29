62b06e139c1df47b00dc3cd542ed10015640fd4c
Matt McGuire <matt.mcguire@metc.state.mn.us>
2021-10-29 13:32:07 -0500

    Parameter tweaks

--- ap_upload_anoka.py
+++ ap_upload_anoka.py
@@ -21,14 +21,20 @@ import getpass
 
 #Get Parameters
 county = sys.argv[1]
+if county == "#":
+    county = "Hennepin"
 dataset = sys.argv[2]
-#----Set working directory when incorporating in script or running from Python-----#
-workingDirectory = "K:\\data_devel\\AddressPoints\\1_Data_Collection\\anoka-county"
-workingDirectory = "K:\\data_devel\\parcels\\1_Data_Collection\\hennepin-county"
-
+if dataset == "#":
+    dataset = "AddressPoints"
+wd = sys.argv[3]
 # Set working directory for Use in Tool
-#workingDirectory = os.path.dirname(os.path.realpath(sys.argv[0]))
-
+workingDirectory = os.path.dirname(os.path.realpath(sys.argv[3]))
+if wd == "#":
+    #----Set working directory when incorporating in script or running from Python-----#
+    workingDirectory = "K:\\data_devel\\{}\\1_Data_Collection\\{}-county".format(dataset,county)
+else:
+    workingDirectory = os.path.dirname(os.path.realpath(wd))
+    
 # Open log file
 logFile = workingDirectory + "\\upload.log"
 report = open(logFile,'w')
@@ -36,14 +42,15 @@ report = open(logFile,'w')
 
 # Get timestamp
 timestamp = time.strftime("%c")
-#report.write("Running script at " + timestamp + "\n")
+report.write("Running script at " + timestamp + "\n")
+report.write("{}, {}\n".format(county, dataset))
 
 
 # Set local variables
 ### ArcGIS Portal Upload Variables ###
 portalurl = "https://arcgis.metc.state.mn.us/portal"
-report.write(sys.executable)
-report.write(sys.version)
+report.write("{}\n".format(sys.executable))
+report.write("{}\n".format(sys.version))
 ### Username and password
 username = "testtemp"
 pwd = keyring.get_password("MetroGISFileUpload", username)
@@ -51,19 +58,23 @@ if not pwd:
     report.write("password not set yet")
     p = getpass.getpass()
     pwd = keyring.set_password("MetroGISFileUpload", username, p)
-else:
-    print("Password!")
+
 portal = GIS(portalurl, username, pwd)
 me = portal.users.me
 
 ### UPLOAD VARIABLES ###
-uploadFilePath = workingDirectory + "\\adp.zip" # Path to local file that will be uploaded to update existing resource
-uploadFilePath = workingDirectory + "\\parcel.zip" # Path to local file that will be uploaded to update existing resource
+if dataset == "AddressPoints":
+    uploadFilePath = workingDirectory + "\\adp.zip" # Path to local file that will be uploaded to update existing resource
+elif dataset == "parcels":
+    uploadFilePath = workingDirectory + "\\parcel.zip" # Path to local file that will be uploaded to update existing resource
+#Check if uploadFilePath exists
 
 #### Begin Upload to Met Council ArcGIS Portal ####
+title = "{} County {}".format(county, dataset)
+tags = "metrogis, {}, {}, test, delete"
 item_properties = {"type": "File Geodatabase",
-                    "title": "Hennepin County Parcels",
-                    "tags":"metrogis,test,delete",
+                    "title": title,
+                    "tags":tags,
                     "snippet":"testing upload of zipped file gdb",
                     "description":"Started with a small file geodatabase. This is a large sized zip file. Eventually testing a large upload and automating it.",
                   "commentsEnabled" : False,
@@ -73,15 +84,12 @@ item_properties = {"type": "File Geodatabase",
 rslt = portal.content.add(item_properties, data=uploadFilePath)
 if rslt:
     # Log full response
-    report.write("Uploaded item{}".format(rslt.homepage))
+    report.write("Uploaded item: {}\n".format(rslt.homepage))
 else:
-    report.write("Upload unsuccessful")
-
-
-
+    report.write("Upload unsuccessful\n")
 
 #### END UPLOAD TO MRCC PORTAL ####
 # Close log and exit
-report.write("\n" + "Script completed")
+report.write("\n" + "Script completed\n----------------------------------")
 report.close()
 sys.exit()
