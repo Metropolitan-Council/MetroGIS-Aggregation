7794b6f526189799f9377fa58decf794784a2eac
Matt McGuire <matt.mcguire@metc.state.mn.us>
2021-11-11 14:16:02 -0600

    Updated to allow using as module

--- uploadscript.py
+++ uploadscript.py
@@ -14,88 +14,87 @@ import requests
 import json
 from arcgis.gis import GIS
 from arcgis.gis import Item
-#from urllib3.exceptions import InsecureRequestWarning #added
 import keyring
 import getpass
 import configparser
 import logging
 
+## Module Variables
+# this is a pointer to the module object instance itself.
+this = sys.modules[__name__]
+this.uploadFilePath = None
+this.portal = None
 # open config file for other parameters
 config = configparser.ConfigParser()
+
 config.read('config.ini')
 
-
-
-#Get Dataset Parameter
-#ADDRESSPOINTS, PARCELS, CENTERLINES
-try:
-    dataset = sys.argv[1].upper()
-except KeyError:
-    dataset = "DEFAULT"
-if dataset == "ADDRESSPOINTS":
-    pass
-elif dataset == "PARCELS":
-    pass
-elif dataset == "CENTERLINES":
-    pass
-elif dataset =="PARKS":
-    pass
-else:
-    print("dataset={}".format(dataset))
-    print("first and only parameter must be one of ADDRESSPOINTS, PARCELS, CENTERLINES, PARKS")
-    exit()
-
-# Open log file
-logFile = config[dataset]['logfile']
-logLevel = int(config[dataset]['loglevel'])
+# create logger object
+logFile = config["DEFAULT"]['logfile']
 logger = logging.getLogger("MetroGISDataPublish")
-logger.setLevel(logLevel)
 fh = logging.FileHandler(logFile)
-#fh.setLevel(logging.DEBUG)
-fh.setLevel(logLevel)
 ch = logging.StreamHandler()
-ch.setLevel(20)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fh.setFormatter(formatter)
-ch.setFormatter(formatter)
-logger.addHandler(fh)
-logger.addHandler(ch)
-
-
-
-#Look for key errors
-portalurl = config[dataset]['portalurl']
-uploadFilePath = config[dataset]['uploadfile']
-username = config[dataset]['User']
-
-timestamp = time.strftime("%c")
-# Log parameters
-logger.info("Dataset {}".format(dataset))
-logger.info("Uploading file: {}".format(uploadFilePath))
-logger.info("Uploading to: {}".format(portalurl))
-logger.info("User: {}".format(username))
-
-# Set local variables
-### ArcGIS Portal Upload Variables ###
-logger.debug("{}".format(sys.executable))
-logger.debug("{}".format(sys.version))
-### Username and password
-pwd = keyring.get_password("MetroGISFileUpload", username)
-if not pwd:
-    logger.info("password not set yet")
-    p = getpass.getpass()
-    pwd = keyring.set_password("MetroGISFileUpload", username, p)
-    logger.info("password saved in keyring")
+def init(dataset):
+    # Open log file
+    logLevel = int(config[dataset]['loglevel'])
+    logger.setLevel(logLevel)
+    #fh.setLevel(logging.DEBUG)
+    fh.setLevel(logLevel)
     
-portal = GIS(portalurl, username, pwd)
-me = portal.users.me
+    ch.setLevel(20)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    logger.addHandler(fh)
+    logger.addHandler(ch)
 
-#Check if uploadFilePath exists
+
+    #Look for key errors
+    portalurl = config[dataset]['portalurl']
+    this.uploadFilePath = config[dataset]['uploadfile']
+    username = config[dataset]['User']
+
+    # Log parameters
+    logger.info("Dataset {}".format(dataset))
+    logger.info("Uploading file: {}".format(this.uploadFilePath))
+    logger.info("Uploading to: {}".format(portalurl))
+    logger.info("User: {}".format(username))
+
+    # Set local variables
+    ### ArcGIS Portal Upload Variables ###
+    logger.debug("{}".format(sys.executable))
+    logger.debug("{}".format(sys.version))
+    ### Username and password
+    pwd = keyring.get_password("MetroGISFileUpload", username)
+    if not pwd:
+        logger.info("password not set yet")
+        p = getpass.getpass()
+        pwd = keyring.set_password("MetroGISFileUpload", username, p)
+        logger.info("password saved in keyring")
+        
+    this.portal = GIS(portalurl, username, pwd)
+
+def validatedataset(dataset):
+    ds = dataset.upper()
+    '''make sure dataset is one of the MetroGIS Aggregated datesets'''
+    validdslist = ["ADDRESSPOINTS", "PARCELS", "CENTERLINES", "PARKS"]
+    if ds in validdslist:
+        return ds
+    else:
+        print("dataset={}".format(dataset))
+        print("first and only parameter must be one of ADDRESSPOINTS, PARCELS, CENTERLINES, PARKS")
+        exit()
 
 #### Begin Upload to Met Council ArcGIS Portal ####
-title = "{}".format(dataset)
-tags = "metrogis, {}, test, delete".format(dataset)
-item_properties = {"type": "File Geodatabase",
+def uploaddataset(dataset):
+    dataset = validatedataset(dataset)
+    init(dataset)
+    uploadFilePath = this.uploadFilePath
+    portal = this.portal
+    #Set up metadata for item
+    title = "{}".format(dataset)
+    tags = "metrogis, {}, test, delete".format(dataset)
+    item_properties = {"type": "File Geodatabase",
                     "title": title,
                     "tags":tags,
                     "snippet":"testing upload of zipped file gdb",
@@ -103,44 +102,64 @@ item_properties = {"type": "File Geodatabase",
                   "commentsEnabled" : False,
                   "overwrite":True
                   }
-logger.debug("Overwrite Property={}".format(item_properties["overwrite"]))
-path = Path(uploadFilePath)
-if path.is_file():
-    try:
-        rslt = portal.content.add(item_properties, data=uploadFilePath)
+    
+    path = Path(uploadFilePath)
+    if path.is_file():
+        try:
+            rslt = portal.content.add(item_properties, data=uploadFilePath)
         
-    except Exception as e:
-        logger.info("{} Upload {} failed".format(dataset, uploadFilePath))
-        logger.info("{}".format(e))
-        #If error code 409. Need to get item and update
-        #update(file, folder_name=None, file_name=None, text=None)
-        #rslt = portal.content.update(item_properties, data=uploadFilePath)
-        rslt=None
-else:
-    logger.warning("{} does not point to a file".format(uploadFilePath))
-    rslt = None
-if rslt:
-    # Log full response
-    logger.debug("new itemid: {}".format(rslt.id))
-    config[dataset]['itemid']=rslt.id
-    with open('config.ini', 'w') as configfile:
-        config.write(configfile)
-    logger.debug("added itemid to config file under {}".format(dataset))
-    logger.info("Uploaded item: {}".format(rslt.homepage))
-else:
-    #try getting item with id and updating item https://developers.arcgis.com/python/api-reference/arcgis.gis.toc.html#arcgis.gis.Item.update
+        except Exception as e:
+            logger.info("{} Upload {} failed".format(dataset, uploadFilePath))
+            logger.info("{}".format(e))
+            #If error code 409. Need to get item and update
+            #update(file, folder_name=None, file_name=None, text=None)
+            #rslt = portal.content.update(item_properties, data=uploadFilePath)
+            rslt=None
+    else:
+        if uploadFilePath == '':
+            logger.warning("uploadFilePath: {} is empty".format(uploadFilePath))
+        else:
+            logger.warning("uploadFilePath: {} does not point to a file".format(uploadFilePath))
+        rslt = None
+    if rslt:
+        # Log full response
+        logger.debug("new itemid: {}".format(rslt.id))
+        config[dataset]['itemid']=rslt.id
+        with open('config.ini', 'w') as configfile:
+            config.write(configfile)
+        logger.debug("added itemid to config file under {}".format(dataset))
+        logger.info("Uploaded item: {}".format(rslt.homepage))
+    else:
+        #try getting item with id and updating item https://developers.arcgis.com/python/api-reference/arcgis.gis.toc.html#arcgis.gis.Item.update
+        try:
+            itemid = config[dataset]['itemid']
+
+            updateitem = portal.content.get(itemid)
+            updateitem.update(data=uploadFilePath)
+        except KeyError:
+            logger.critical("No Item ID in config file. {} upload unsuccessful!".format(dataset))
+        except Exception as e:
+            logger.critical("{} upload unsuccessful!".format(dataset))
+            logger.warning("{}".format(e))
+    #### END UPLOAD TO Met Council ArcGIS PORTAL ####
+
+    # Close log and exit
+    logger.info("-----------------Script completed----------------------------------\n")
+    logger.removeHandler(fh)
+    logger.removeHandler(ch)
+
+    #report.close()
+
+def main(dataset):
+    uploaddataset(dataset)
+
+if __name__ == "__main__":
+    #Get Dataset Parameter
+    #ADDRESSPOINTS, PARCELS, CENTERLINES
     try:
-        itemid = config[dataset]['itemid']
-        updateitem = portal.content.get(itemid)
-        updateitem.update(data=uploadFilePath)
+        ds = sys.argv[1].upper()
+        dataset = validatedataset(ds)
     except KeyError:
-        logger.critical("{} upload unsuccessful!".format(dataset))
-
-#### END UPLOAD TO Met Council ArcGIS PORTAL ####
-# Close log and exit
-logger.info("-----------------Script completed----------------------------------\n")
-#report.close()
-sys.exit()
-
-
+        dataset = "DEFAULT"
+    main(dataset)
 
